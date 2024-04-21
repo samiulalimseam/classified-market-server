7fb16cfd826305a638ec52e97862843df03a9914
samiul alim <samiulalimseam@gmail.com>
2024-04-21 15:59:33 +0600

    added build static files

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT | 5000 ;
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
+
+// Serve the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
 // middleware
 app.use(cors());
@@ -181,9 +186,16 @@ run().catch(error=> console.log(error));
 
 
 
-app.get('/', async (req,res)=>{
+app.get('/api', async (req,res)=>{
    res.send('Server Running');
 })
+
+// Serve the React app for all other routes
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+
+ 
 app.listen(port,()=>{
    console.log('Server running on port',port);
 })
