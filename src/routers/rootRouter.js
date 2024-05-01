1b21b7f46a2519409143ab0c5b3007c11eb810a6
mehedi <mehedikhan3281@gmail.com>
2024-05-01 23:02:13 +0600

    added toast on signup/login

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.flmxcne.mongodb.net/?retryWrites=true&w=majority` | process.env?.DB_URI;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.flmxcne.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
